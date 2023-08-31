4996a2208a6e6a52ab03760fc646f68e44acb22a
euichanhwang <pine6971@naver.com>
2023-08-31 20:30:02 +0900

    example: `싱글톤` 범위
    별도 설정을 하지 않을 경우 스프링은 한 개의 빈 객체만을 생성하며, 이때 빈 객체는 싱글톤 범위를 갖는다.

--- chapter02/src/main/java/me/euichan/chap02/Main.java
+++ chapter02/src/main/java/me/euichan/chap02/Main.java
@@ -8,9 +8,9 @@ public class Main {
 		// 설정 정보를 이용해서 빈 객체를 생성한다.
 		AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppContext.class);
 		// 빈 객체를 제공한다.
-		Greeter g = ac.getBean("greeter", Greeter.class); // 빈 객체의 이름, 빈 객체의 타입
-		String msg = g.greet("스프링");
-		System.out.println(msg);
+		Greeter g1 = ac.getBean("greeter", Greeter.class);
+		Greeter g2 = ac.getBean("greeter", Greeter.class);
+		System.out.println("(g1 == g2) = " + (g1 == g2));
 		ac.close();
 	}
 }
