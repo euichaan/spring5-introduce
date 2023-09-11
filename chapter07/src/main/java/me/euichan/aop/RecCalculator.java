96f8dce3972ca8a8e9e30ea72ae133800c0003dd
euichanhwang <pine6971@naver.com>
2023-09-11 15:45:56 +0900

    example: 두 계산기의 실행 시간을 출력하기

--- chapter07/src/main/java/me/euichan/aop/RecCalculator.java
+++ chapter07/src/main/java/me/euichan/aop/RecCalculator.java
@@ -4,9 +4,15 @@ public class RecCalculator implements Calculator {
 
 	@Override
 	public long factorial(final long num) {
-		if (num == 0) {
-			return 1;
+		long start = System.currentTimeMillis();
+		try {
+			if (num == 0) {
+				return 1;
+			} else
+				return num * factorial(num - 1);
+		} finally {
+			long end = System.currentTimeMillis();
+			System.out.printf("RecCalculator.factorial(%d) 실행 시간 = %d\n", num, (start - end));
 		}
-		else return num * factorial(num - 1);
 	}
 }
