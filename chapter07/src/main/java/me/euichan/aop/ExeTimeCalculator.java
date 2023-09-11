5e94a96c31349617ed1fd71b4571404de50029c8
euichanhwang <pine6971@naver.com>
2023-09-11 15:49:42 +0900

    example: 프록시 객체를 이용해 실행 시간을 측정한다

---
+++ chapter07/src/main/java/me/euichan/aop/ExeTimeCalculator.java
@@ -0,0 +1,20 @@
+package me.euichan.aop;
+
+public class ExeTimeCalculator implements Calculator {
+
+	private Calculator delegate;
+
+	public ExeTimeCalculator(final Calculator delegate) {
+		this.delegate = delegate;
+	}
+
+	@Override
+	public long factorial(final long num) {
+		long start = System.nanoTime();
+		long result = delegate.factorial(num);
+		long end = System.nanoTime();
+		System.out.printf("%s.factorial(%d) 실행 시간 = %d\n",
+			delegate.getClass().getName(), num, (end - start));
+		return result;
+	}
+}
